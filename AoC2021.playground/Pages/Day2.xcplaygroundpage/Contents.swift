c6df0620ede9959cbb457df0654b4fcbd1740c54
umpusten <12965914+UMPUSTEN@users.noreply.github.com>
2021-12-02 15:30:35 +0100

    using the reduce function to calculate distance

--- AoC2021.playground/Pages/Day2.xcplaygroundpage/Contents.swift
+++ AoC2021.playground/Pages/Day2.xcplaygroundpage/Contents.swift
@@ -1,25 +1,42 @@
 import Foundation
 let input = inputDay2.components(separatedBy: .newlines).map { $0.components(separatedBy: .whitespacesAndNewlines) }
-let inputDict = input.reduce(into: [String: Int]()) { result, element in
-    result[element[0]] = (result[element[0]] ?? 0) + Int(element[1])!
-}
-//Part 1
-print(inputDict["forward"]!*(inputDict["down"]!-inputDict["up"]!))
+//let inputDict = input.reduce(into: [String: Int]()) { result, element in
+//    result[element[0]] = (result[element[0]] ?? 0) + Int(element[1])!
+//}
+//
+//print(inputDict["forward"]!*(inputDict["down"]!-inputDict["up"]!))
 
-//Part 2
-var position = (horizontal: 0, depth: 0, aim: 0)
-input.forEach { direction in
-    guard let distance = Int(direction[1]) else { return }
+//Part 1
+let part1 = input.reduce((horizontal: 0, depth: 0)) { position, direction in
+    guard let distance = Int(direction[1]) else { return (position.horizontal, position.depth) }
+    
     switch direction[0] {
     case "forward":
-        position.horizontal += distance
-        position.depth += distance*position.aim
+        return (position.horizontal + distance, position.depth)
     case "up":
-        position.aim -= distance
+        return (position.horizontal, position.depth - distance)
     case "down":
-        position.aim += distance
-    default: break
+        return (position.horizontal, position.depth + distance)
+    default:
+        return (position.horizontal, position.depth)
     }
 }
-print(position.horizontal*position.depth)
 
+part1.depth * part1.horizontal
+
+//Part 2
+let part2 = input.reduce((horizontal: 0, depth: 0, aim: 0)) { position, direction in
+    guard let distance = Int(direction[1]) else { return (position.horizontal, position.depth, position.aim) }
+    
+    switch direction[0] {
+    case "forward":
+        return (position.horizontal + distance, position.depth + distance * position.aim, position.aim)
+    case "up":
+        return (position.horizontal, position.depth, position.aim - distance)
+    case "down":
+        return (position.horizontal, position.depth, position.aim + distance)
+    default:
+        return (position.horizontal, position.depth, position.aim)
+    }
+}
+print(part2.horizontal*part2.depth)
