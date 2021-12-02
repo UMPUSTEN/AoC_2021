3485cfef3ff5bb600e05b23ea2f6ad44b490ddcd
umpusten <12965914+UMPUSTEN@users.noreply.github.com>
2021-12-02 15:15:50 +0100

    cleanup

--- AoC2021.playground/Pages/Day2.xcplaygroundpage/Contents.swift
+++ AoC2021.playground/Pages/Day2.xcplaygroundpage/Contents.swift
@@ -1,32 +1,25 @@
 import Foundation
 let input = inputDay2.components(separatedBy: .newlines).map { $0.components(separatedBy: .whitespacesAndNewlines) }
-
 let inputDict = input.reduce(into: [String: Int]()) { result, element in
     result[element[0]] = (result[element[0]] ?? 0) + Int(element[1])!
 }
-
-
 //Part 1
 print(inputDict["forward"]!*(inputDict["down"]!-inputDict["up"]!))
-//Note: Actually I did not expact this to work. Since "up" should not be able to be < 0? ..
 
 //Part 2
-var position = (horizontal: 0, depth: 0)
-var aim = 0
+var position = (horizontal: 0, depth: 0, aim: 0)
 input.forEach { direction in
     guard let distance = Int(direction[1]) else { return }
     switch direction[0] {
     case "forward":
         position.horizontal += distance
-        position.depth += distance*aim
+        position.depth += distance*position.aim
     case "up":
-//        position.depth -= distance
-        aim -= distance
+        position.aim -= distance
     case "down":
-//        position.depth += distance
-        aim += distance
+        position.aim += distance
     default: break
     }
-    print(position)
 }
 print(position.horizontal*position.depth)
+
