4ddb8588ebe9a3772f937919dbcd4479f158cc20
umpusten <12965914+UMPUSTEN@users.noreply.github.com>
2021-12-02 09:13:51 +0100

    remove unnecessary time measuring

--- AoC2021.playground/Pages/Day1.xcplaygroundpage/Contents.swift
+++ AoC2021.playground/Pages/Day1.xcplaygroundpage/Contents.swift
@@ -4,20 +4,15 @@ let input = inputDay1.components(separatedBy: .newlines).map { Int($0)! }
 var counter = 0
 
 //Part 1
-var start = CFAbsoluteTimeGetCurrent()
-
 for x in 1...input.count-1 {
     counter += input[x] > input[x-1] ? 1 : 0
 }
-print(CFAbsoluteTimeGetCurrent() - start, "ms")
 print(counter)
 
 //Part 2
 counter = 0
-start = CFAbsoluteTimeGetCurrent()
 for x in 1...input.count-3 {
     counter += input[x...x+2].reduce(0, +) > input[x-1...x+1].reduce(0, +) ? 1 : 0
 }
-print(CFAbsoluteTimeGetCurrent() - start, "ms")
 print(counter)
 
